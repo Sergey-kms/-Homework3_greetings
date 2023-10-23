7d79a24d44322c3dfc26e88fa3fa7740258445f6
penkinsergey <penkinsergeykms@gmail.com>
2023-10-23 21:39:30 +0800

    greetings1

---
+++ Homework3_greetings/src/main/java/Homework3_greetings.java
@@ -0,0 +1,11 @@
+public class Homework3_greetings {
+    public static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+    public static void main (String... args) {
+        greetings();
+    }
+}
